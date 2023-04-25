bf61ba2ede25218e7cff46d79c17f05d56f07fc8
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-25 15:32:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,18 +7,15 @@ const promises = [];
 
 // Add 5 promises to the array
 for (let i = 0; i < 5; i++) {
-  const randomTime = Math.floor(Math.random() * 5) + 1; // Generate a random time between 1 and 5 seconds
-  const promise = new Promise(resolve => {
-    setTimeout(() => {
-      resolve(`Promise ${i+1} resolved after ${randomTime} second(s)`);
-    }, randomTime * 1000);
-  });
-  promises.push(promise);
-}
+        const time = Math.floor(Math.random() * 5) + 1;
+        const promise = new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(`Promise ${i + 1} resolved in ${time} seconds`);
+          }, time * 1000);
+        });
+        promises.push(promise);
+      }
 
-Promise.any(promises)
-  .then(result => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = result;
-  })
-  .catch(error => console.log(error));
+      Promise.any(promises).then((result) => {
+        document.getElementById("output").innerText = result;
+      });
